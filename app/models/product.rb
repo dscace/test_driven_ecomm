7fbaa64e5f74678ca6a0db5e2d4403c53d4a8c4c
dscace <derek.scace@gmail.com>
2017-01-25 11:12:08 -0700

    Add 4 passing tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   validates :name, presence: true
   validates :quantity, numericality: {greater_than_or_equal_to: 0}
+  validates_format_of :price, with: /\A\d+\.?\d{0,2}\z/
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,8 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
- 	it "doesn't allow a product's name to be nil" do
-     macbook = FactoryGirl.build(:product, quantity: nil)
-     expect(macbook).to_not be_valid
+ 	  describe "name" do
+	    it "cannot be nil" do
+	      product = FactoryGirl.build(:product, name: nil)
+	    end
+	  end
+  
+	  describe "quantity" do
+	    it "cannot be nil" do
+	      product = FactoryGirl.build(:product, quantity: nil)
+
+	      expect(product).to_not be_valid
+	    end
+	  end
+
+
+	    it "cannot be negative" do
+	      product = FactoryGirl.build(:product, quantity: -1)
+
+	      expect(product).to_not be_valid
+	    end
+
+	  describe "price" do
+    	it "cannot have 3 or more numbers after the decimal point" do
+      	product = FactoryGirl.build(:product, price: 78.456)
+
+      	expect(product).to_not be_valid
+    	end
+  	end
+
+  	
   end
-end
